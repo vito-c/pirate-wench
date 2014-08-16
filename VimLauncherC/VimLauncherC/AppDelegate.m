1f629d3efedcea3438b6012081fac0a6ea96f7b2
GodFather <vcutten@zynga.com>
2014-08-16 00:46:39 -0700

    cleaned up the code a bunch and it now works a lot better

--- VimLauncherC/VimLauncherC/AppDelegate.m
+++ VimLauncherC/VimLauncherC/AppDelegate.m
@@ -53,159 +53,33 @@ NSString *const BUNDLE_ID = @"com.googlecode.iterm2";
     
     iTermITermApplication *iTerm = [SBApplication applicationWithBundleIdentifier:@"com.googlecode.iterm2"];
     [[NSWorkspace sharedWorkspace] launchApplication:@"/Applications/iTerm.app"];
-
-    if( [self vimUnityRunning:@"UNITY"] )
-        NSLog(@"UNITY vim running");
-    else
-        NSLog(@"UNITY vim NOT running");
     
-    
-    
-    if ( [self selectSession:@"UNITY (vim)"] && [self vimUnityRunning:@"UNITY"] ) {
-        //send command to go to file and linenumber through any terminal
+    if ( [self selectSession:@"UNITY (vim)"] ) {
+        if([self vimUnityRunning:@"UNITY"] ){
+            //send command to go to file and linenumber through vim clientserver terminal
+            [self vimOpenFile:filename line:lineString server:@"UNITY"];
+        } else {
+            //A different vim is currently running and vim unity is not running
+            [self backgroundVimProcess];
+            [NSThread sleepForTimeInterval:1];
+            [self runHacks:filename line:lineString];
+        }
+        //TODO: edge case missing where your current vim is not vim unity and vim unity is running in the bkg
     } else if( [self selectSession:@"UNITY (bash)"] ){
         [self backgroundProcess];
-        if( [self vimUnityRunning:@"UNITY"] ){
-            //reactivate
-        } else {
-            //launch new vim
-        }
+        [NSThread sleepForTimeInterval:1];
+        [self runHacks:filename line:lineString];
     } else {
         //launch new session
         [[iTerm currentTerminal] launchSession:@"Default Session"];
         iTerm.currentTerminal.currentSession.name = @"UNITY";
-        NSString *targetDir = [self getPath:filename ];
-        [self writeToCurrentTerm:[NSString stringWithFormat:@"cd %@",targetDir]];
-        if( [@"-1" isEqualToString:lineString])
-            [self writeToCurrentTerm:[NSString stringWithFormat:@"/usr/local/bin/vim --servername UNITY %@",filename]];
-         else
-             [self writeToCurrentTerm:[NSString stringWithFormat:@"/usr/local/bin/vim --servername UNITY %@ %@",lineString,filename]];
+        [NSThread sleepForTimeInterval:1];
+        [self runHacks:filename line:lineString];
     }
     
-    //usr/local/bin/vim --servername UNITY +14 /Users/vcutten/Documents/unity/TestProject/Assets/Hexagon.cs
-    
-    
-    NSLog(@"Current term: %@", iTerm.currentTerminal.currentSession.name);
     [NSApp terminate:nil];
     return TRUE;
-    
-    
-    
-    
-    NSArray* apps = [NSRunningApplication
-                     runningApplicationsWithBundleIdentifier:@"com.googlecode.iterm2"];
-    [(NSRunningApplication*)[apps objectAtIndex:0]
-     activateWithOptions: NSApplicationActivateAllWindows];
- 
-//    NSLog(@"Current song is %@", [[iTerm currentTerminal] sessions]);
-//    iTerm.currentTerminal.sessions
-    /*
-    NSTask *unixTask = [[NSTask alloc] init];
-    [unixTask setStandardOutput:newPipe];
-    [unixTask setStandardInput:[NSPipe pipe]];
-    [unixTask setStandardError:errPipe];
-    [unixTask setLaunchPath:@"/usr/local/bin/vim"];
-    [unixTask setArguments: [ NSArray arrayWithObjects:
-                             @"--servername",
-                             @"UNITY",
-                             @"--remote-silent",
-                             lineString,
-                             filename,
-                             nil]  ];
-    */
-    
-    
-    NSPipe *newPipe = [NSPipe pipe];
-    NSPipe *errPipe = [NSPipe pipe];
-    NSFileHandle *readHandle = [newPipe fileHandleForReading];
-    NSFileHandle *errorHandle = [errPipe fileHandleForReading];
-
-     NSTask *unixTask = [[NSTask alloc] init];
-     [unixTask setStandardOutput:newPipe];
-     [unixTask setStandardInput:[NSPipe pipe]];
-     [unixTask setStandardError:errPipe];
-     [unixTask setLaunchPath:@"~/.pirate-setup/pirate-wench/vim-launcher"];
-     [unixTask setArguments: [ NSArray arrayWithObjects:
-                              lineString,
-                              filename, nil] ];
-    [unixTask launch];
-    
-    NSString *stdOut = [[NSString alloc] initWithData:[readHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
-    NSString *errOut = [[NSString alloc] initWithData:[errorHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
-    NSLog(@"Dumping std out: %@", stdOut );
-    NSLog(@"Dumping std err: %@", errOut );
-    
-    
-    if( [errOut length] == 0 ){
-        NSLog(@"error out is 0");
-    } else {
-        NSLog(@"OMG there is an error");
-    }
-    if( [stdOut length] == 0 ){
-        NSLog(@"Standard out is 0");
-    }
-
-    
-    /*
-    NSTask *unixTask = [[NSTask alloc] init];
-    [unixTask setStandardOutput:newPipe];
-    [unixTask setStandardInput:[NSPipe pipe]];
-    [unixTask setStandardError:errPipe];
-    [unixTask setLaunchPath:@"/usr/local/bin/vim"];
-    [unixTask setArguments: [ NSArray arrayWithObjects:
-                                @"--servername",
-                                @"UNITY",
-                                @"--remote-send", @"", nil] ];
-
-    [unixTask launch];
-    [unixTask waitUntilExit];
-    
-    NSString *stdOut = [[NSString alloc] initWithData:[readHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
-    NSString *errOut = [[NSString alloc] initWithData:[errorHandle readDataToEndOfFile] encoding:NSUTF8StringEncoding];
-    NSLog(@"Dumping std out: %@", stdOut );
-    NSLog(@"Dumping std err: %@", errOut );
-    
-    [[NSWorkspace sharedWorkspace] launchApplication:@"/Applications/iTerm.app"];
-    NSArray* apps = [NSRunningApplication
-                     runningApplicationsWithBundleIdentifier:@"com.googlecode.iterm2"];
-    [(NSRunningApplication*)[apps objectAtIndex:0]
-     activateWithOptions: NSApplicationActivateAllWindows];
-    
-    if( [errOut length] == 0 ){
-        NSLog(@"error out is 0");
-    } else {
-        NSLog(@"OMG there is an error");
-    }
-    if( [stdOut length] == 0 ){
-        NSLog(@"Standard out is 0");
-    }
-    */
-    //~/.pirate-setup/pirate-wench/vim-launcher
-    
-    /*
-    NSString *output = [[NSTask launchedTaskWithLaunchPath:@"/usr/local/bin/vim"
-                            arguments: [ NSArray arrayWithObjects:
-                                    @"--servername",
-                                    @"UNITY",
-                                    @"--remote-send", @"", nil] ] waitUntilExit];
-
-     */ 
-    /*
-    NSTask *task = [[NSTask alloc] init];
-    [task setLaunchPath:@"/usr/local/bin/vim"];
-    
-    NSArray *arguments = [NSArray arrayWithObject:filename];
-    [task setArguments:arguments];
-    for (id element in arguments) {
-        NSLog ( @"Arguments: %@", element );
-    }
-    
-    [task launch];
-    */
-    [NSApp terminate:nil];
-    
-    return TRUE;
-}
+ }
 - (void)writeToCurrentTerm:(NSString *)text {
      NSTask *unixTask2 = [[NSTask alloc] init];
      [unixTask2 setLaunchPath:@"/usr/bin/osascript"];
@@ -221,6 +95,17 @@ NSString *const BUNDLE_ID = @"com.googlecode.iterm2";
     [unixTask2 setArguments: [ NSArray arrayWithObjects:
                               @"/Users/vcutten/.pirate-setup/pirate-wench/sendctrlz.scpt",
                               nil] ];
+    [unixTask2 waitUntilExit];
+    [unixTask2 launch];
+}
+
+- (void)backgroundVimProcess{
+    NSTask *unixTask2 = [[NSTask alloc] init];
+    [unixTask2 setLaunchPath:@"/usr/bin/osascript"];
+    [unixTask2 setArguments: [ NSArray arrayWithObjects:
+                              @"/Users/vcutten/.pirate-setup/pirate-wench/sendESCctrlz.scpt",
+                              nil] ];
+    [unixTask2 waitUntilExit];
     [unixTask2 launch];
 }
 
@@ -228,29 +113,48 @@ NSString *const BUNDLE_ID = @"com.googlecode.iterm2";
     return [self vimSendCMD:@"\"\"" server:@"UNITY"];
 }
 
+- (void)runHacks:(NSString *) filename
+            line: (NSString *) lineNumber
+{
+    NSString *targetDir = [self getPath:filename ];
+    [self writeToCurrentTerm:[NSString stringWithFormat:@"cd %@",targetDir]];
+    if( [@"-1" isEqualToString:lineNumber])
+        [self writeToCurrentTerm:[NSString stringWithFormat:@"/usr/local/bin/vim --servername UNITY %@",filename]];
+    else
+        [self writeToCurrentTerm:[NSString stringWithFormat:@"/usr/local/bin/vim --servername UNITY %@ %@",lineNumber,filename]];
+
+}
 
 - (BOOL)vimOpenFile: (NSString *) fileName
                line: (NSString *) lineNumber
              server: (NSString *) serverName
 {
-    //usr/local/bin/vim --servername UNITY --remote-silent +14 /Users/vcutten/Documents/unity/TestProject/Assets/Hexagon.cs
+    
     NSTask *unixTask = [[NSTask alloc] init];
     NSPipe *newPipe = [NSPipe pipe];
     NSPipe *errPipe = [NSPipe pipe];
 //    NSFileHandle *errorHandle = [errPipe fileHandleForReading];
-    
     [unixTask setStandardOutput:newPipe];
     [unixTask setStandardInput:[NSPipe pipe]];
     [unixTask setStandardError:errPipe];
     [unixTask setLaunchPath:@"/usr/local/bin/vim"];
     [unixTask waitUntilExit];
-    [unixTask setArguments: [ NSArray arrayWithObjects:
-                             @"--servername",
-                             serverName,
-                             @"--remote-silent",
-                             lineNumber,
-                             fileName,
-                             nil] ];
+    if( [@"-1" isEqualToString:lineNumber]){
+        [unixTask setArguments: [ NSArray arrayWithObjects:
+                                 @"--servername",
+                                 serverName,
+                                 @"--remote-silent",
+                                 fileName,
+                                 nil] ];
+    } else {
+        [unixTask setArguments: [ NSArray arrayWithObjects:
+                                 @"--servername",
+                                 serverName,
+                                 @"--remote-silent",
+                                 lineNumber,
+                                 fileName,
+                                 nil] ];
+    }
     [unixTask launch];
     
 //    NSString *errMsg = @"E247: no registered server named \"UNITY\": Send failed.\n";
--- sendESCctrlz.scpt
+++ sendESCctrlz.scpt
@@ -5,6 +5,9 @@ tell application "iTerm"
 		set mysession to (current session)
 		tell mysession
 			tell application "System Events"
+				keystroke key code 53
+			end tell
+			tell application "System Events"
 				keystroke "z" using {control down}
 			end tell
 		end tell
